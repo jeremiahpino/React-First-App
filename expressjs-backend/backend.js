6c1d3b27eca8d3a77483e7d6964f54ad8a605e7d
jeremiahpino <jpino1999@gmail.com>
2022-09-27 22:15:04 -0700

    get users by name

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,17 +1,26 @@
+// impoprt express module
 const express = require('express');
+
+// create an instance of express
 const app = express();
 const port = 5001;
 
+// process incoming data in JSON format
 app.use(express.json());
 
+// see a '/', return Hello World!
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+
+// backend server listening to incoming http requests on specified port number
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+
+// list of users 
 const users = { 
     users_list :
     [
@@ -43,6 +52,28 @@ const users = {
     ]
  }
 
- app.get('/users', (req, res) => {
-    res.send(users);
+// retrieving all users 
+//  app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+
+
+// query a certain user by name
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+// find a particular name in the user's list
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+// finished step 4 COMMIT CHANGES to backendBranch
