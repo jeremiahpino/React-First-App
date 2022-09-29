b8fa35b26ce8ac184e6454b868486bb9d55118be
jeremiahpino <jpino1999@gmail.com>
2022-09-29 10:44:17 -0700

    code commented

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-// process incoming data in JSON format
+// process incoming data in JSON format (Middleware)
 app.use(express.json());
 
 // see a '/', return Hello World!
